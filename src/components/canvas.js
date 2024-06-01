b0f7d3e37f2f98b2f1738533d544dd232fdafad9
Johnathan Chiu <43973975+johnathanchiu@users.noreply.github.com>
2024-06-01 01:23:34 -0400

    Option to connect points via Bezier curves (#1)
    * add bezier curves, need to clean up outlier points
    
    * allow both curve and dashed lines
    
    * get dashed vs. curves working properly, needed to add reference to the object

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { setupWebcam, teardownWebcam } from "../lib/video";
 import { useAnimationFrame } from "../lib/hooks/animation";
 import { createKeyMap, drawHands } from "../lib/pose";
@@ -15,13 +15,18 @@ export async function setupCanvas(video, canvasID) {
 }
 
 export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
-  const [drawCtx, setDrawCtx] = useState(null);
-  const [floatCtx, setFloatCtx] = useState(null);
   const [isStreaming, setStreaming] = useState(false);
   const [brushSize, setBrushSize] = useState(2);
   const [drawingPoints, setDrawingPoints] = useState([]);
+  const [isDrawing, setDrawing] = useState(false);
+  const [isDashed, setDashed] = useState(true);
 
-  const draw = (hands) => {
+  const [indicatorCtx, setIndicatorCtx] = useState(null);
+  const [drawCtx, setDrawCtx] = useState(null);
+  const [floatCtx, setFloatCtx] = useState(null);
+  const currentCanvasRef = useRef(null);
+
+  const drawIndicators = (hands) => {
     for (let i = 0; i < hands.length; i++) {
       const hand = hands[i];
       let indexKeypoint = hand.keypoints.index_finger_tip;
@@ -32,41 +37,62 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
           [indexKeypoint.y, thumbKeypoint.y],
         ]) < 20.0
       ) {
-        // setDrawingPoints((previousState) => [
-        //   ...previousState,
-        //   { x: indexKeypoint.x, y: indexKeypoint.y },
-        // ]);
+        setDrawingPoints((previousState) => [
+          ...previousState,
+          { x: indexKeypoint.x, y: indexKeypoint.y },
+        ]);
+        setDrawing(true);
 
-        drawCtx.beginPath();
-        drawCtx.arc(
+        currentCanvasRef.current.beginPath();
+        currentCanvasRef.current.arc(
           indexKeypoint.x,
           indexKeypoint.y,
           brushSize,
           0,
           2 * Math.PI
         );
-        drawCtx.fill();
+        currentCanvasRef.current.fill();
+      } else {
+        setDrawing(false);
       }
-      // else {
-      //   setDrawingPoints([]);
-      // }
     }
   };
 
-  // useEffect(() => {
-  //   if (drawingPoints.length > 1) {
-  //     drawCtx.beginPath();
-  //     drawCtx.moveTo(
-  //       drawingPoints[drawingPoints.length - 2].x,
-  //       drawingPoints[drawingPoints.length - 2].y
-  //     );
-  //     drawCtx.lineTo(
-  //       drawingPoints[drawingPoints.length - 1].x,
-  //       drawingPoints[drawingPoints.length - 1].y
-  //     );
-  //     drawCtx.stroke();
-  //   }
-  // }, [drawingPoints]);
+  const draw = (points) => {
+    drawCtx.beginPath();
+    drawCtx.moveTo(points[0].x, points[0].y);
+
+    if (points.length === 2) {
+      // For two points, just draw a straight line
+      drawCtx.lineTo(points[1].x, points[1].y);
+      return;
+    }
+
+    for (let i = 1; i < points.length - 2; i += 2) {
+      if (i + 2 < points.length) {
+        drawCtx.bezierCurveTo(
+          points[i].x,
+          points[i].y,
+          points[i + 1].x,
+          points[i + 1].y,
+          points[i + 2].x,
+          points[i + 2].y
+        );
+      }
+    }
+
+    // If there are an odd number of points, handle the last segment
+    if (points.length % 2 === 0) {
+      let lastPoint = points[points.length - 1];
+      drawCtx.lineTo(lastPoint.x, lastPoint.y);
+    } else {
+      let cp = points[points.length - 2];
+      let endPoint = points[points.length - 1];
+      drawCtx.quadraticCurveTo(cp.x, cp.y, endPoint.x, endPoint.y);
+    }
+
+    drawCtx.stroke();
+  };
 
   useEffect(() => {
     async function initialize() {
@@ -74,7 +100,7 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
         videoRef.current = await setupWebcam();
         console.log("webcam setup!");
       }
-      if (!drawCtx || !floatCtx) {
+      if (!drawCtx || !floatCtx || !indicatorCtx) {
         const drawingCanvas = await setupCanvas(
           videoRef.current,
           "draw-canvas"
@@ -83,8 +109,19 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
           videoRef.current,
           "float-canvas"
         );
+        const indicatorCanvas = await setupCanvas(
+          videoRef.current,
+          "indicator-canvas"
+        );
         setDrawCtx(drawingCanvas);
         setFloatCtx(floatingCanvas);
+        setIndicatorCtx(indicatorCanvas);
+
+        if (isDashed) {
+          currentCanvasRef.current = drawingCanvas;
+        } else {
+          currentCanvasRef.current = indicatorCanvas;
+        }
         console.log("canvas setup!");
       }
     }
@@ -95,9 +132,10 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
         videoRef.current = null;
         console.log("webcam teardown!");
       }
-      if (drawCtx || floatCtx) {
+      if (drawCtx || floatCtx || indicatorCtx) {
         setDrawCtx(null);
         setFloatCtx(null);
+        setIndicatorCtx(null);
         console.log("canvas teardown!");
       }
     }
@@ -108,6 +146,14 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
       destroy();
     }
   }, [isStreaming]);
+
+  useEffect(() => {
+    if (isDashed) {
+      currentCanvasRef.current = drawCtx;
+    } else {
+      currentCanvasRef.current = indicatorCtx;
+    }
+  }, [isDashed]);
 
   useAnimationFrame(async (delta) => {
     let hands = await detector.estimateHands(videoRef.current, {
@@ -122,8 +168,28 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
       videoRef.current.videoHeight
     );
     drawHands(hands, floatCtx);
-    draw(hands);
-  }, !!(isStreaming && isModelLoaded && videoRef.current && floatCtx && drawCtx));
+    drawIndicators(hands);
+  }, !!(isStreaming && isModelLoaded && videoRef.current));
+
+  useEffect(() => {
+    if (!isDrawing) {
+      if (indicatorCtx) {
+        indicatorCtx.clearRect(
+          0,
+          0,
+          videoRef.current.width,
+          videoRef.current.height
+        );
+      }
+
+      if (!isDashed) {
+        if (drawingPoints.length >= 2) {
+          draw(drawingPoints);
+        }
+      }
+      setDrawingPoints([]);
+    }
+  }, [isDrawing]);
 
   return (
     <div className="flex h-screen">
@@ -131,7 +197,7 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
       <div className="bg-gray-200 w-1/12 p-4">
         {isModelLoaded && videoRef && (
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 w-full"
             onClick={() => {
               setStreaming((prevState) => !prevState);
             }}
@@ -140,19 +206,39 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
           </button>
         )}
         {isStreaming && (
-          <button
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4"
-            onClick={() => {
-              drawCtx.clearRect(
-                0,
-                0,
-                videoRef.current.width,
-                videoRef.current.height
-              );
-            }}
-          >
-            Clear Canvas!
-          </button>
+          <div>
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 w-full"
+              onClick={() => {
+                drawCtx.clearRect(
+                  0,
+                  0,
+                  videoRef.current.width,
+                  videoRef.current.height
+                );
+                indicatorCtx.clearRect(
+                  0,
+                  0,
+                  videoRef.current.width,
+                  videoRef.current.height
+                );
+              }}
+            >
+              Clear Canvas!
+            </button>
+            <button
+              className={`text-white font-bold py-2 px-4 rounded mt-4 w-full ${
+                isDashed
+                  ? "bg-green-500 hover:bg-green-700"
+                  : "bg-gray-500 hover:bg-gray-700"
+              }`}
+              onClick={() => {
+                setDashed((prevState) => !prevState);
+              }}
+            >
+              {isDashed ? "Solid" : "Dashed"}
+            </button>
+          </div>
         )}
       </div>
 
@@ -161,8 +247,22 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
         <canvas
           style={{
             position: "absolute",
-            display: isStreaming ? "block" : "none",
             backgroundColor: "transparent",
+            display: isStreaming ? "block" : "none",
+            transform: "scaleX(-1)",
+            zIndex: 2,
+            borderRadius: "1rem",
+            boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
+            width: "100%",
+            height: "100%",
+          }}
+          id="draw-canvas"
+        />
+        <canvas
+          style={{
+            position: "absolute",
+            backgroundColor: "transparent",
+            display: isStreaming ? "block" : "none",
             transform: "scaleX(-1)",
             zIndex: 1,
             borderRadius: "1rem",
@@ -170,7 +270,7 @@ export default function CanvasComponent({ videoRef, detector, isModelLoaded }) {
             width: "100%",
             height: "100%",
           }}
-          id="draw-canvas"
+          id="indicator-canvas"
         />
         <canvas
           style={{
